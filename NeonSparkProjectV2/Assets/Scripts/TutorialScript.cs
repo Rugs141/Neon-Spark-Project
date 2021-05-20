d1b3034ee3da8309b041ddab647e843a16201839
Penji <DeansMalotsis@gmail.com>
2021-05-20 13:48:00 +1000

    idk

---
+++ NeonSparkProjectV2/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TutorialScript : MonoBehaviour
+{
+    private PlayerScript PlayerScript;
+    public GameObject tutorialSpaceBarPrompt;
+    private Animator TutorialSpaceAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerScript.closestDashPoint != null)
+        {
+            //tutorialSpaceBarPrompt;
+        }
+    }
+}
